ab731e0d3d8ee7806f37f647dd421843be6ff561
Artem Barysh <abarysh@thingsboard.io>
2025-02-04 12:07:28 +0200

    improved formatting and moved check to recordActivityInternal.

--- common/transport/transport-api/src/main/java/org/thingsboard/server/common/transport/service/DefaultTransportService.java
+++ common/transport/transport-api/src/main/java/org/thingsboard/server/common/transport/service/DefaultTransportService.java
@@ -1,12 +1,12 @@
 /**
  * Copyright © 2016-2024 The Thingsboard Authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -732,13 +732,13 @@ public class DefaultTransportService extends TransportActivityManager implements
 
     @Override
     public void recordActivity(TransportProtos.SessionInfoProto sessionInfo) {
-        if(sessionInfo != null) {
-            recordActivityInternal(sessionInfo);
-        }
+        recordActivityInternal(sessionInfo);
     }
 
     private void recordActivityInternal(TransportProtos.SessionInfoProto sessionInfo) {
-        onActivity(toSessionId(sessionInfo), sessionInfo, getCurrentTimeMillis());
+        if (sessionInfo != null) {
+            onActivity(toSessionId(sessionInfo), sessionInfo, getCurrentTimeMillis());
+        }
     }
 
     @Override
